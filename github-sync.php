cfef10265a70d16469bde4b67944ec05d1e11317
AnthonyB66 <35492714+AnthonyB66@users.noreply.github.com>
2019-06-02 18:08:31 +0200

    Update github-sync.php

--- github-sync.php
+++ github-sync.php
@@ -1,1 +1,24 @@
-<?php `git pull`;
+<?php 
+$fullPostData = file_get_contents('php://input');
+#$data = json_decode($fullPostData);
+#var_dump($data);
+try
+{
+  $payload = json_decode($fullPostData);
+}
+catch(Exception $e)
+{
+  exit(0);
+}
+
+//log the request
+file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+
+if ($payload->ref === 'refs/heads/master')
+{
+  echo "Mise en production\n";
+  // path to your site deployment script
+ $output = system("./build.sh");
+
+ # var_dump($output);
+}
